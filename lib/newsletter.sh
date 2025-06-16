e210b8095665b9fdf19ff35959b33364f4aaa0ce
Alexander Panov <panov@royalzsoftware.de>
2025-06-16 23:52:33 +0200

    feat: usage

--- lib/newsletter.sh
+++ lib/newsletter.sh
@@ -113,3 +113,18 @@ function send {
     send_mail $1 "$compiled_template"
     echo "Sent email issue $2 at $NOW" >> $SUBSCRIBED_DIR/$1
 }
+
+function usage {
+    echo ""
+    echo "Author: Alexander Panov <panov@royalzsoftware.de>"
+    echo "GitHub: https://github.com/royalzsoftware/newsletter"
+    echo ""
+    echo "Newsletter CLI"
+    echo ""
+    echo "  newsletter list                   - print out newsletter subscribers"
+    echo "  newsletter subscribe <email>      - subscribe to the newsletter"
+    echo "  newsletter confirm <email> <code> - confirm subscription"
+    echo "  newsletter unsubscribe <email>    - unsubscribe from newsletter"
+    echo "  newsletter send <email> <issue>   - send <issue> to <email>"
+    echo ""
+}
